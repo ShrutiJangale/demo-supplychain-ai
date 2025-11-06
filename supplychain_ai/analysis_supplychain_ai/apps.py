2e27cd120c4c05681b0862776966d8144a83ef03
ShrutiJangale <shruti.j@ergobite.com>
2025-11-06 17:25:55 +0530

    Initial commit - Supply Chain AI project

---
+++ supplychain_ai/analysis_supplychain_ai/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnalysisSupplychainAiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'analysis_supplychain_ai'
---
+++ supplychain_ai/supplychain_ai/config.py
@@ -0,0 +1,2 @@
+questions_file=r"D:\demoForSupplyChain\questions.json"
+prompts_file=r"D:\demoForSupplyChain\prompts.json"
